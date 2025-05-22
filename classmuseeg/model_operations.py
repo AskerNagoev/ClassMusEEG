4220740e8014dcd2766364973bd94e01f1df0052
AskerNagoev <80641933+AskerNagoev@users.noreply.github.com>
2025-05-22 04:22:22 +0300

    Update model_operations.py

--- classmuseeg/model_operations.py
+++ classmuseeg/model_operations.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, log_loss
 import seaborn as sns
 import matplotlib.pyplot as plt
 import optuna
